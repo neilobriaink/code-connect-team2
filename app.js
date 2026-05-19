f73e820c24a078217a306ab6d1d09aed50c4728b
Harry Horder <1harryhjh@gmail.com>
2026-05-19 10:37:41 +0100

    Task complete (#13)
    * Taskc complete
    
    * Delete button readded
    
    ---------
    
    Co-authored-by: Harry Horder <harryhorder@gmail.com>

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var employeesRouter = require('./routes/employees');
+var rolesRouter = require('./routes/roles')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/employees', employeesRouter);
+app.use('/roles', rolesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
